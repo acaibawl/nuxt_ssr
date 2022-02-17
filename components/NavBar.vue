8e8f6cd7791e3794c6131adb0492c7b096cff48f
acaiibawl <hagiya0719@gmail.com>
2022-02-17 17:28:40 +0900

    Code Splittingでアクセスするページごとに分割されたJSファイルを作成することと、リンクごとにプリフェッチのon/offができることを確認

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,7 +2,7 @@
   <div>
     <nav>
       <nuxt-link to="/">Home</nuxt-link>
-      <nuxt-link to="/about">About</nuxt-link>
+      <nuxt-link to="/about" no-prefetch>About</nuxt-link>
     </nav>
   </div>
 </template>
